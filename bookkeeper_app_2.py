187945e3ecfc8da3a54f79f69e466ce7faa684ff
Viet Son Pham <viet-son.pham@traderepublic.com>
2024-08-31 20:09:46 +0200

    update booking logic

--- bookkeeper_app_2.py
+++ bookkeeper_app_2.py
@@ -50,7 +50,7 @@ def process_images(image_list, app_password_input, bar):
                         Der Gesamtbetrag soll ein Komma zur Abtrennung von Euro und Cent haben.
                         Der Rechnungstyp wird durch einen Begriff codiert und bestimmt sich durch folgende Regeln:
                         - Wenn die Rechnung von einer Tankstelle kommt oder Benzin oder Diesel enthält, verwende den Begriff "Tankstelle" als Rechnungstyp.
-                        - Wenn die Rechnung das Wort Floristenbedarf enthält, verwende den Begriff "Floristenbedarf" als Rechnungstyp.
+                        - Wenn der Unternehmensname das Wort 'Floristenbedarf' enthält, verwende den Begriff "Floristenbedarf" als Rechnungstyp.
                         - Verwende in allen anderen Fällen den Begriff "Rest" als Rechnungstyp.
                         Gebe mir die Antwort in folgender Struktur:
                         {
