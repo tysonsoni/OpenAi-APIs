848ae59407923e59f050ddaedb2fb74b5386b214
Tyson Soni <tysonpython92@gmail.com>
2024-09-15 21:19:18 +0200

    change to "rote Zahl"

--- bookkeeper_app.py
+++ bookkeeper_app.py
@@ -45,7 +45,7 @@ def process_images(image_list, app_password_input, bar):
                                 {
                                     "type": "text",
                                     "text": """
-                                    Wie lautet der Unternehmensname, der Gesamtbetrag, der Rechnungstyp, das Datum und die handschriftlich eingekreiste Zahl in der folgenden Rechnung? 
+                                    Wie lautet der Unternehmensname, der Gesamtbetrag, der Rechnungstyp, das Datum und die rote Zahl in der folgenden Rechnung? 
                                     Das Datum soll das Format TTMM haben.
                                     Der Gesamtbetrag soll ein Komma zur Abtrennung von Euro und Cent haben.
                                     Der Rechnungstyp wird durch einen Begriff codiert und bestimmt sich durch folgende Regeln:
@@ -58,7 +58,7 @@ def process_images(image_list, app_password_input, bar):
                                         "Gesamtbetrag" : 'String',
                                         "Datum" : 'String',
                                         "Rechnungstyp" : 'String',
-                                        "eingekreiste Zahl" : 'Integer'
+                                        "rote Zahl" : 'Integer'
                                     }
                                     """
                                 },
@@ -92,11 +92,11 @@ def process_images(image_list, app_password_input, bar):
                                     "Rechnungstyp": {
                                         "type": "string"
                                     },
-                                    "eingekreiste Zahl": {
+                                    "rote Zahl": {
                                         "type": "integer"
                                     }
                                 },
-                                "required": ["Unternehmensname", "Gesamtbetrag", "Datum", "Rechnungstyp", "eingekreiste Zahl"],
+                                "required": ["Unternehmensname", "Gesamtbetrag", "Datum", "Rechnungstyp", "rote Zahl"],
                                 "additionalProperties": False
                             }
 
@@ -113,7 +113,7 @@ def process_images(image_list, app_password_input, bar):
                     "Soll/Haben-Kennzeichen": "H",
                     "Konto": 1371,
                     "Belegdatum": resp["Datum"],
-                    "Belegfeld 1": resp["eingekreiste Zahl"],
+                    "Belegfeld 1": resp["rote Zahl"],
                     "Buchungstext": resp["Unternehmensname"],
                     "Festschreibung": 0
                 }
